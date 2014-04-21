8c12468c4fd0d1a5ade0a7b8b7929f6bf7206bfa
keshavnandan <nandan.keshav55@gmail.com>
2014-04-21 17:12:25 +0530

    Revert "Modified main function with day"
    This reverts commit 5daf8bb728bb1a11256d8006bac1805b69ab0bda.

--- src/main/java/oracle/javacloud/gitdemo/App.java
+++ src/main/java/oracle/javacloud/gitdemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Git Demo from Keshav  on Tuesday!" );
+        System.out.println( "Git Demo from Keshav!" );
     }
 }
